1ae1ea80c87d41aa8503da7f1438eb19a8857fe9
mrcoding <39582062+MingRay98@users.noreply.github.com>
2019-10-18 10:55:52 +0800

    Updates

---
+++ precache-manifest.3ad615cfa838cabbdff14dddfc7299ba.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cca8f0d241833e971b83e7f03d8798e5",
+    "url": "/message-board/index.html"
+  },
+  {
+    "revision": "b34a443044d56080b94a",
+    "url": "/message-board/static/css/main.53c147da.chunk.css"
+  },
+  {
+    "revision": "90b4620260398372f397",
+    "url": "/message-board/static/js/2.00696775.chunk.js"
+  },
+  {
+    "revision": "b34a443044d56080b94a",
+    "url": "/message-board/static/js/main.ca27ea13.chunk.js"
+  },
+  {
+    "revision": "d0da3e9c0eb932fe5027",
+    "url": "/message-board/static/js/runtime-main.ac32eb0b.js"
+  }
+]);
